f36631e2243f13a7bb20e5ec477f1bd43afa17fa
ander7even <ander7even@mail.ru>
2021-12-01 22:54:36 +0900

    Добавлена Задача 2

---
+++ Task_6.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6._2
+{
+    class Program
+    {
+        static void Main(string[] args)  //проверка является ли строка палиндромом
+        {
+            Console.WriteLine("Введите предложение, без знаков припинания:");
+            string startString = Console.ReadLine();
+            startString = startString.Replace(" ", "");    //убрали пробелы
+            startString = startString.ToUpper();           // перевели в верхний регистр
+
+            char[] stringArray = startString.ToCharArray();
+
+
+            Array.Reverse(stringArray);
+
+            string finalString = new string(stringArray);
+
+            if (finalString == startString)
+            {
+                Console.WriteLine("Введеное предложение является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Введеное предложение не является палиндромом");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
