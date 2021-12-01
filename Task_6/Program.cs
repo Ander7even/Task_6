17778cc277aa08767bd5fdb1d290f61f25393b24
ander7even <ander7even@mail.ru>
2021-12-01 20:32:09 +0900

    Добавьте файлы проекта.

---
+++ Task_6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)  //самое длинное слово в предложении
+        {
+            {
+                Console.WriteLine("Введите предложение, без знаков припинания:");
+                string startString = Console.ReadLine();
+                
+                string[] stringArray = startString.Split();
+
+                int maxLenghts = 0;
+                string result = "";
+
+                foreach (string asd in stringArray)
+                {
+                    if (maxLenghts < asd.Length)
+                    {
+                        maxLenghts = asd.Length;
+                        result = asd;
+                    }
+
+                }
+                
+                Console.WriteLine("{0} - самое длинное слово в вашем предложении.", result);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
